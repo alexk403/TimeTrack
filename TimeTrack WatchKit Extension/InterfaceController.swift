7d1691fb164f2cd95f8b62479a85beedd1ce1ae4
Alex Kapustka <alexk4@me.com>
2016-07-22 22:19:24 -0500

    started implementation of background tasks

--- TimeTrack WatchKit Extension/InterfaceController.swift
+++ TimeTrack WatchKit Extension/InterfaceController.swift
@@ -27,5 +27,64 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+	
+	@IBAction func timerSwitchEnabled(_ value: Bool) {
+		if value {
+			print("switch on")
+			/*let date = Date()
+			let calendar = Calendar.current
+			let components = calendar.components(.second, from: date)
+			let delayedStart: Double = (Double) (60 - components.second!)
+			let dispatchTime: DispatchTime = DispatchTime.now() + Double(Int64(delayedStart * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+			
+			DispatchQueue.main.after(when: dispatchTime, execute: {
+				let userInfo = ["reason" : "background update"]
+				WKExtension.shared().scheduleBackgroundRefresh(withPreferredDate: Date(timeIntervalSinceNow: 5.0), userInfo: userInfo, scheduledCompletion: { error in
+					if (error == nil) {
+						print("background task scheduled")
+					}
+				})
+			})*/
+			let userInfo = ["reason" : "background update"]
+			WKExtension.shared().scheduleBackgroundRefresh(withPreferredDate: Date(timeIntervalSinceNow: 5.0), userInfo: userInfo, scheduledCompletion: { error in
+				print("well im here")
+				if (error == nil) {
+					print("background task scheduled")
+				}
+			})
+		}
+	}
+	
+	/*@IBOutlet var timerLabel: WKInterfaceLabel!
+	
+	var mainTimer: Timer! = nil
+	
+	func timerDone(_ timer:Timer){
+		WKInterfaceDevice.current().play(.stop)
+		//print("Done!")
+	}
+	
+	@IBAction func switchEnabled(_ value: Bool) {
+		if(value){
+			//print("switch on")
+			//print(myTimer.valid)
+			let date = Date()
+			let calendar = Calendar.current
+			let components = calendar.components([.second], from: date)
+			let delayedStart:Double=(Double)(60-components.second!)
+			let dispatchTime: DispatchTime = DispatchTime.now() + Double(Int64(delayedStart * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+			DispatchQueue.main.after(when: dispatchTime, execute: {
+				self.myTimer = Timer.scheduledTimer(timeInterval: 5.0, target: self, selector: #selector(InterfaceController.timerDone(_:)), userInfo: nil, repeats: true)
+			})
+		}
+		else{
+			// invalidate timer here
+			//print(myTimer.valid)
+			myTimer.invalidate()
+			//print("switch off")
+			//print(myTimer.valid)
+		}
+		
+	}*/
 
 }
